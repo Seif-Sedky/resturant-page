82a5b9735ea571bf6571ee86d1953e12e8922421
Seif Sedky <Seif.alaa1231@gmail.com>
2025-07-20 14:56:50 +0300

    Add menu

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,15 @@
-import './menu.css';
+import menuImg from './imgs/menu.png'
 
-function displayMenu(){
+function displayMenu() {
+    const content = document.querySelector("#content");
+    let menu = document.createElement('img');
+    menu.src = menuImg;
+
+    content.style.display = 'flex';
+    content.style.justifyContent = 'center';
+    content.style.alignItems = 'center';
+
+    content.appendChild(menu);
 
 }
 
